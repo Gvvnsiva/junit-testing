b6118f718fc6e0bec955d43e12cbb73f92fe1619
GUMMELLA <v.gummella@dxc.com>
2020-07-25 17:15:38 +0530

    firstcommit

---
+++ parameter/src/main/java/org/dxc/ioc/parameterised_project/parameter/App.java
@@ -0,0 +1,13 @@
+package org.dxc.ioc.parameterised_project.parameter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ parameter/src/test/java/org/dxc/parameterizedtest/Calculator.java
@@ -0,0 +1,13 @@
+package org.dxc.parameterizedtest;
+
+public class Calculator {
+
+	int add(int a,int b) {
+		return a+b;
+	}
+	int sub(int a,int b) {
+		return a+b;
+	
+	}
+
+}
